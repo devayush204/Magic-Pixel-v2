fc3f3a5b31717f5b24d9f57c78759db9f475c384
ayush <ayushbhatt204@gmail.com>
2025-02-27 20:11:01 +0530

    changes

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_magic-pixel"
+      uploadPreset="jsm_magicpixel"
       options={{
         multiple: false,
         resourceType: "image",
